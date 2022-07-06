cbdc8ba036ec0836b8bd48554a9a7d818b06f2fe
Yait-el- <aitelkob@users.noreply.github.com>
2022-07-06 17:31:15 +0100

    add some themplate to

--- label_studio/core/urls.py
+++ label_studio/core/urls.py
@@ -80,7 +80,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
-    path('docs/api/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('docs/api/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('docs/', RedirectView.as_view(url='/static/docs/public/guide/introduction.html', permanent=False)),
 
     path('admin/', admin.site.urls),
